48b82add35962cc54bc943a6ccbf68fe8c162a21
Karl Fundenberger <karl.fundenberger@gmail.com>
2021-12-13 14:01:47 -0600

    remove second fxrand() call

--- index.js
+++ index.js
@@ -1,27 +1,10 @@
-// these are the variables you can use as inputs to your algorithms
+// "Tumbling Glyphs" by nuflark
+// https://twitter.com/enuflark
+// Special thanks to Liam Egan (https://twitter.com/neuromantic6) for help with debugging! 
+// 2021
+
 console.log(fxhash)   // the 64 chars hex number fed to your algorithm
 console.log(fxrand()) // deterministic PRNG function, use it instead of Math.random()
-
-// note about the fxrand() function 
-// when the "fxhash" is always the same, it will generate the same sequence of
-// pseudo random numbers, always
-
-//----------------------
-// defining features
-//----------------------
-// You can define some token features by populating the $fxhashFeatures property
-// of the window object.
-// More about it in the guide, section features:
-// [https://fxhash.xyz/articles/guide-mint-generative-token#features]
-//
-// window.$fxhashFeatures = {
-//   "Background": "Black",
-//   "Number of lines": 10,
-//   "Inverted": true
-// }
-
-
-
 
 const firstDoodle = document.querySelector('.first-doodle');
 firstDoodle.grid = "40";
@@ -35,16 +18,6 @@ secondDoodle.seed = fxrand();
 console.log(secondDoodle.grid);
 console.log(secondDoodle.seed);
 
-
-
-// Features
-// Display is functional but not quite accurate yet
-
-
-
-var x = (Math.floor(fxrand()*10)+1);
-console.log(x);
-
 function getFeatureString(value) {
   if (value === 1) return "mint"
   if (value === 2) return "pale purp"
@@ -55,3 +28,5 @@ function getFeatureString(value) {
 window.$fxhashFeatures = {
   "Theme": getFeatureString(x)
 }
+
+// x is defined in index.html
